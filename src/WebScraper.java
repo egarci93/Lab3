eadf6fe56715c46f8911bcd378c0c1f643378208
egarci93 <egarci93@illinois.edu>
2018-02-07 18:17:56 -0600

    completed word count.

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -1,5 +1,30 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class WebScraper {
     public static void main (String[] unused) {
-        System.out.println("Ran");
+        String webName = "http://tgftp.nws.noaa.gov/data/raw/fz/fzus53.klot.srf.lot.txt";
+        String webPage = urlToString(webName);
+        String[] a = webPage.split( "\\S+"); // "\\S+" counts all spaces.
+        System.out.println(a.length);
+
+    }
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        }catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
     }
 }
